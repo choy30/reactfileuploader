0c236c8f49116620d72c037887cc5dbf33d25c8a
devYeonjun <alcls0026@gmail.com>
2022-06-04 20:20:48 +1200

    Add css on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-const  App = () => {
-  return (
-    <div className="App">
-
-    </div>
-  );
-}
+const App = () => (
+<div className='container mt-4'>
+    <h4 className='display-4 text-center mb-4'>
+      <i className='fab fa-react' /> React File Upload
+    </h4>
+  </div>
+)
 
 export default App;
