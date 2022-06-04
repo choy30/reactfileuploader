db0d022a95c1a92eeb4ce9a96a7c28e99ee26946
devYeonjun <alcls0026@gmail.com>
2022-06-04 20:32:03 +1200

    Add onchange & onsubmit fnc

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios'
 
 const FileUpload = () => {
+	const [file, setFile] = useState();
+	const [fileName, setFileName] = useState("Choose File");
+
+	const onChange = (e) => {
+		setFile(e.target.files[0]);
+		setFileName(e.target.files[0].name);
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		const formData = new FormData();
+		formData.append("file", file);
+	};
+
 	return (
 		<>
-			<form>
+			<form onSubmit={onSubmit}>
 				<div className="custom-file mb-4">
-					<input type="file" className="custom-file-input" id="customFile" />
+					<input
+						type="file"
+						className="custom-file-input"
+						id="customFile"
+						onChange={onChange}
+					/>
 					<label className="custom-file-label" htmlFor="customFile">
-						Choose file
+						{fileName}
 					</label>
 				</div>
 				<input
