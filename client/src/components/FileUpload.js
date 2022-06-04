b4b96db51909d7b1a7548da57706fe86c2bab726
devYeonjun <alcls0026@gmail.com>
2022-06-04 20:48:10 +1200

    Add message component

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+import Message from "./Message";
+
 const FileUpload = () => {
 	const [file, setFile] = useState();
 	const [fileName, setFileName] = useState("Choose File");
 	const [uploadedFile, setUploadedFile] = useState({});
+	const [message, setMessage] = useState("");
 
 	const onChange = (e) => {
 		setFile(e.target.files[0]);
@@ -26,17 +29,20 @@ const FileUpload = () => {
 			const { fileName, filePath } = res.data;
 
 			setUploadedFile({ fileName, filePath });
+
+			setMessage("File Uploaded");
 		} catch (error) {
 			if (error.response.status === 500) {
-				console.log("There was a problem with the server");
+				setMessage("There was a problem with the server");
 			} else {
-				console.log(error.response.data.msg);
+				setMessage(error.response.data.msg);
 			}
 		}
 	};
 
 	return (
 		<>
+			{message ? <Message msg={message} /> : null}
 			<form onSubmit={onSubmit}>
 				<div className="custom-file mb-4">
 					<input
